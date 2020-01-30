c56c263da1b453a4214dc995fc49520ae8878db9
Brad Chesworth <bradley.chesworth@bbc.co.uk>
2020-01-30 12:52:25 +0000

    Created IBAction that runs when button is tapped
    checks to see if its the correct answer, shows a alart and then carries on with the game.

--- flag-guesser/ViewController.swift
+++ flag-guesser/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
         askQuestion()
     }
 
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         
@@ -41,6 +41,24 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct!"
+            score += 1
+        } else {
+            title = "Wrong!"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
     }
 }
 
